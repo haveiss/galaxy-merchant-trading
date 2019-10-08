f200c16f7c4c6754092ca94853d030b67c91227e
Haveiss Muhamad <to@haveiss.com>
2019-10-08 21:44:59 +0700

    ToRoman implemented

--- numeral/roman.go
+++ numeral/roman.go
@@ -2,9 +2,34 @@ package numeral
 
 import "fmt"
 
+var romanUnits = []struct {
+	symbol string
+	value  int
+}{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"CD", 400},
+	{"C", 100},
+	{"XC", 90},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
+}
+
 // ToRoman Convert Arabic Number to Roman
 func ToRoman(i int) string {
 	roman := ""
+	for _, unit := range romanUnits {
+		for i >= unit.value {
+			roman = roman + unit.symbol
+			i = i - unit.value
+		}
+	}
 	return roman
 }
 
